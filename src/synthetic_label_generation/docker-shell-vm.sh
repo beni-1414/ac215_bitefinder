e39fe87beede241227b8b4198063bff2618e949b
Benet Fite <benet_fite@hms.harvard.edu>
2025-10-16 15:18:52 -0400

    feat: create docker-shells and adapt dockerfile to main theme

--- src/synthetic_label_generation/docker-shell-vm.sh
+++ src/synthetic_label_generation/docker-shell-vm.sh
@@ -2,12 +2,16 @@
 
 set -e # Exit on error
 
+source ../../env.dev # Read the settings file
 export IMAGE_NAME="synthetic-label-generation"
 
 # Build Docker image
 docker build -t $IMAGE_NAME -f Dockerfile .
 
-# Run Docker container with mounted volumes
+# Run Docker container with mounted volumes for data and secrets
 docker run --rm --name $IMAGE_NAME -ti \
     -v "$(pwd)":/app \
+    -v "$(pwd)"/$DATA_DIR:/app/data \
+    -e GCP_PROJECT=$GCP_PROJECT \
+    -e GCP_BUCKET_NAME=$GCP_BUCKET_NAME \
     $IMAGE_NAME
