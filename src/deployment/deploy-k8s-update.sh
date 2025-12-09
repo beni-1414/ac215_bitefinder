f575220d7de70f8194cf43d6fd1dddbc06eaed35
Benet Fite <bfite2020@gmail.com>
2025-12-08 22:01:05 -0500

    feat: Try to fix the CD /deploy-app

--- src/deployment/deploy-k8s-update.sh
+++ src/deployment/deploy-k8s-update.sh
@@ -1,8 +1,7 @@
-cd deploy_images
+cd /deploy_images
 pulumi stack select agent
 pulumi up --stack agent -y
 
-cd ..
-cd deploy_k8s
+cd /deploy_k8s
 pulumi stack select agent
 pulumi up --stack agent -y
